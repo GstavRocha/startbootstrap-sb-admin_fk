9c66a630defc2d2698b7926046cf5b7ae03b4659
GustavoDesk <rocha.gustavo@escolar.ifrn.edu.br>
2024-02-17 14:40:47 -0300

    change models to api

---
+++ ApiRequests/External_APIs/External_Routes/routesGets.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nomes;
+function getNames() {
+    console.log(nomes);
+}
+getNames();
+console.log('teste');
---
+++ ApiRequests/External_APIs/External_Routes/routesGets.ts
@@ -0,0 +1,9 @@
+import {InstrumentoFinanceiro} from "../Models/InstrumentoFinanceiro";
+import axios from "axios";
+let nomes: InstrumentoFinanceiro["nome"];
+
+function getNames(){
+    console.log(nomes)
+}
+getNames();
+console.log('teste')
